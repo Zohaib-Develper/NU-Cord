2b72bdb43963e21570f1b87a7f23bd52c2527c83
Zohaib Musharaf <zohaib.codemaster@gmail.com>
2025-04-14 15:46:08 +0500

    Refactored code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/HomePage";
 import SignUp from "./pages/SignUpPage";
-import Login from "./pages/LoginPage"
+import Login from "./pages/LoginPage";
 function App() {
   return (
     <>
       {/* <LandingPage/> */}
       {/* <HomePage /> */}
       {/* <HomePage/> */}
-      {/* <SignUp /> */}
-      <Login/>
+      <SignUp />
+      {/* <Login/> */}
     </>
   );
 }
